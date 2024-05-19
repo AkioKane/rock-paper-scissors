6d99c5b3fe82f1d5a213b8754d7f9a5a3fcfc7a2
Akio <aakocherga@mail.ru>
2024-05-20 00:27:39 +0700

    [Added] logic program in app.js

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const buttons = document.querySelectorAll('.button-simbol');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function playSelection(event) {
+            const buttonId = event.target.id;
+            return GetHumanChoice(buttonId);
+        });
+    });
+});
+
 function GetComputerChoice(max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
-function GetHumanChoice() {
+function GetHumanChoice(humanChoice) {
     console.log(
         "\n1 - Rock\n" +
         "2 - Papper\n" +
         "3 - Scissors"
     );
 
-    let number = prompt("Send number!");
+    let choiceHuman = humanChoice;
+    let result = 0
     
-    if (number == 1) {
-        return 1;
+    if (choiceHuman == "rock") {
+        result =  1;
     }
-    if (number == 2) {
-        return 2;
+    if (choiceHuman == "paper") {
+        result = 2;
     }
-    if (number == 3) {
-        return 3;
+    if (choiceHuman == "scissors") {
+        result = 3;
     }
+
+    return playRound(result, GetComputerChoice(3))
 }
 
 function playRound (humanChoice, computerChoice) {
@@ -50,21 +64,6 @@ function playRound (humanChoice, computerChoice) {
     console.log(`Human score: ${humanScore}\n` + `Computer Score: ${computerScore}`)
 }
 
-function playGame(rounds) {
-    for (i=0; i<=rounds; i++) {
-        const humanSelection = GetHumanChoice();
-        const computerSelection = GetComputerChoice(3);
-        playRound(humanSelection, computerSelection);
-    }
-}
-
 let humanScore = 0;
 let computerScore = 0;
 
-playGame(1)
-
-function playSelection(event) {
-    
-}
-
-const buttons = document.querySelectorAll("button")
