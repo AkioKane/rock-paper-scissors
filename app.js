dd3ae2cf51800892693610357ea5d318f6b287cd
Akio <aakocherga@mail.ru>
2024-07-03 13:15:56 +0700

    [refactor] Refactoring code

--- app.js
+++ app.js
@@ -1,62 +1,70 @@
+let humanScore = 0
+let computerScore = 0
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const buttons = document.querySelectorAll('.button-simbol');
 
     buttons.forEach(button => {
         button.addEventListener('click', function playSelection(event) {
             const buttonId = event.target.id;
-            return GetHumanChoice(buttonId);
+            let game = new Game(humanScore, computerScore)
+            return game.getHumanChoice(buttonId);
         });
     });
 });
 
-function GetComputerChoice(max) {
-    return Math.floor(Math.random() * max) + 1;
-}
+class Game {
+    construcor(humanScore, computerScore) {
+        this.humanScore = humanScore;
+        this.computerScore = computerScore;
 
-function GetHumanChoice(humanChoice) {
-    let choiceHuman = humanChoice;
-    let result = 0
+    }
+
+    getComputerChoice(max) {
+        return Math.floor(Math.random() * max) + 1;
+    }
+
+    getHumanChoice(humanChoice) {
+        let choiceHuman = humanChoice;
+        let result = 0
+        
+        if (choiceHuman == "rock") {
+            result =  1;
+        }
+        if (choiceHuman == "paper") {
+            result = 2;
+        }
+        if (choiceHuman == "scissors") {
+            result = 3;
+        }
     
-    if (choiceHuman == "rock") {
-        result =  1;
-    }
-    if (choiceHuman == "paper") {
-        result = 2;
-    }
-    if (choiceHuman == "scissors") {
-        result = 3;
+        return this.playRound(result, this.getComputerChoice(3))
     }
 
-    return playRound(result, GetComputerChoice(3))
+    playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            return console.log("Ничья");
+        }
+    
+        else if (
+            humanChoice == 1 && computerChoice == 2 || 
+            humanChoice == 2 && computerChoice == 3 ||
+            humanChoice == 3 && computerChoice == 1
+        ) {
+            this.humanScore += 1;
+            console.log(`Human score: ${this.humanScore}\n` + `Computer Score: ${this.computerScore}`);
+            return console.log("Human win!");
+        }
+    
+        else if (
+            humanChoice == 1 && computerChoice == 3 || 
+            humanChoice == 2 && computerChoice == 1 ||
+            humanChoice == 3 && computerChoice == 2
+        ) {
+            this.computerScore + 1;
+            console.log(`Human score: ${this.humanScore}\n` + `Computer Score: ${this.computerScore}`);
+            return console.log("Computer win!");
+        }
+        console.log(`Human score: ${this.humanScore}\n` + `Computer Score: ${this.computerScore}`)
+    }
 }
-
-function playRound (humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        return console.log("Ничья");
-    }
-
-    else if (
-        humanChoice == 1 && computerChoice == 2 || 
-        humanChoice == 2 && computerChoice == 3 ||
-        humanChoice == 3 && computerChoice == 1
-    ) {
-        humanScore += 1;
-        console.log(`Human score: ${humanScore}\n` + `Computer Score: ${computerScore}`);
-        return console.log("Human win!");
-    }
-
-    else if (
-        humanChoice == 1 && computerChoice == 3 || 
-        humanChoice == 2 && computerChoice == 1 ||
-        humanChoice == 3 && computerChoice == 2
-    ) {
-        computerScore += 1;
-        console.log(`Human score: ${humanScore}\n` + `Computer Score: ${computerScore}`);
-        return console.log("Computer win!");
-    }
-    console.log(`Human score: ${humanScore}\n` + `Computer Score: ${computerScore}`)
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
